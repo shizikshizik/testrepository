9f2a7dcd620f934eadcdee6f4283493f672162a1
shizikshizik <shizik666@gmail.com>
2016-01-04 23:00:44 +0300

    Signed-off-by: shizikshizik <shizik666@gmail.com>

--- src/com/javarush/test/level18/lesson05/task01/Solution.java
+++ src/com/javarush/test/level18/lesson05/task01/Solution.java
@@ -14,19 +14,18 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream("d:/1.txt");
+        FileInputStream inputStream = new FileInputStream("c:/data.txt");
         // Создаем поток-записи-байт-в-файл
-        FileOutputStream outputStream = new FileOutputStream("d:/2.txt");
+        FileOutputStream outputStream = new FileOutputStream("c:/result.txt");
 
-
+        if (inputStream.available() > 0) {
             //читаем весь файл одним куском
             byte[] buffer = new byte[inputStream.available()];
             int count = inputStream.read(buffer);
             outputStream.write(buffer, 0, count);
-
+        }
 
         inputStream.close();
-        outputStream.flush();
         outputStream.close();
     }
 }
